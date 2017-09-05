2064311037e8fe56e00c9ee56a2120df9ca73405
James Nelson <jamesn@ho-mbp-861.local>
2017-09-05 10:16:44 -0700

    Fixing default region, extending days to grab a full list

--- list_expiring_elbs.py
+++ list_expiring_elbs.py
@@ -23,14 +23,14 @@ UTC = UTC()
 # List server certificates through the pagination interface
 
 def get_expiring_certs(botoconn, days):
-  utc_date_30 = datetime.now(UTC) + timedelta(days)
+  utc_date = datetime.now(UTC) + timedelta(days)
   expiringcerts = defaultdict(list)
   paginator = botoconn.get_paginator('list_server_certificates')
   for response in paginator.paginate():
     for certificate in response['ServerCertificateMetadataList']:
       expire_date = certificate['Expiration']
-      if utc_date_30 > certificate['Expiration']:
-        expiringcerts[certificate['Arn']].append(certificate['Expiration'])
+      if utc_date > certificate['Expiration']:
+        expiringcerts[certificate['Arn']].append({'Expiration':certificate['Expiration']})
   return expiringcerts
 
 # Get ELBs that have listeners matching expired certs
@@ -44,7 +44,7 @@ def get_expiring_elbs(botoconn, certs):
         if listener['Listener']['Protocol'] == 'HTTPS':
           sslcertarn = listener['Listener']['SSLCertificateId']
           if sslcertarn == "Invalid-Certificate":
-            expiringelbs[elb['DNSName']].append('Invalid Cert')
+            expiringelbs[elb['DNSName']].append('Invalid-Certificate')
           if sslcertarn in certs:
             expiringelbs[elb['DNSName']].append(sslcertarn)
   return expiringelbs
@@ -61,29 +61,38 @@ def get_expiring_albs(botoconn, certs):
           for certificate in listener['Certificates']:
             sslcertarn = certificate['CertificateArn']
             if sslcertarn == "Invalid-Certificate":
-              expiringalbs[alb['DNSName']].append('Invalid Cert')
+              expiringalbs[alb['DNSName']].append('Invalid-Certificate')
             if sslcertarn in certs:
               if not sslcertarn[:11] == "arn:aws:acm":
                 expiringalbs[alb['DNSName']].append(sslcertarn)
   return expiringalbs
 
-def print_data (balancers, expiring_certs):
+def print_data(balancers, expiring_certs):
   for balancer in balancers:
     for item in balancers[balancer]:
-      print ("%s,%s,%s" % (balancer,item,expiring_certs[item][0]))
+      if item == "Invalid-Certificate":
+        print("%s,%s,%s" % (balancer, item, ""))
+      else:
+        for expdate in expiring_certs[item]:
+          print("%s,%s,%s" % (balancer, item, expdate['Expiration']))
 
-# Create our global boto3 clients (iam, ec2)
-iam = boto3.client('iam')
-ec2 = boto3.client('ec2')
 
-#IAM Certs are global in the account
-expiring_certs = get_expiring_certs(iam, 300)
+if __name__ == "__main__":
 
-for RegionList in ec2.describe_regions()['Regions']:
-  elb = boto3.client('elb', region_name=RegionList['RegionName'])
-  alb = boto3.client('elbv2', region_name=RegionList['RegionName'])
-  expiringelbs = get_expiring_elbs(elb, expiring_certs)
-  expiringalbs = get_expiring_albs(alb, expiring_certs)
-  print_data(expiringelbs, expiring_certs)
-  print_data(expiringalbs, expiring_certs)
-  
+  # Create our global boto3 clients (iam, ec2)
+  iam = boto3.client('iam', region_name='us-east-1')
+  ec2 = boto3.client('ec2', region_name='us-east-1')
+
+  # IAM Certs are global in the account, 
+  expiring_certs = get_expiring_certs(iam, 3650)
+
+  # Loop through each region, getting all ELB
+  # and ALB listeners, checking against the
+  # expired ssl list generated 
+  for RegionList in ec2.describe_regions()['Regions']:
+    elbconn = boto3.client('elb', region_name=RegionList['RegionName'])
+    albconn = boto3.client('elbv2', region_name=RegionList['RegionName'])
+    expiringelbs = get_expiring_elbs(elbconn, expiring_certs)
+    expiringalbs = get_expiring_albs(albconn, expiring_certs)
+    print_data(expiringelbs, expiring_certs)
+    print_data(expiringalbs, expiring_certs)
